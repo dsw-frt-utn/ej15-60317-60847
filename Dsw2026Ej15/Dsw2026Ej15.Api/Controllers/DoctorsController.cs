059055867507bb2f1b459cc1becbf11d48ac8f8c
vickyortegaf <victoriaortegafernandez@alu.frt.utn.edu.ar>
2026-06-18 21:29:40 -0300

    Ultimos 3 endpoints

--- Dsw2026Ej15/Dsw2026Ej15.Api/Controllers/DoctorsController.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Api/Controllers/DoctorsController.cs
@@ -1,27 +1,27 @@
 ﻿//using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Mvc;
-using Dsw2026Ej15.Domain.Interfaces;
 using Dsw2026Ej15.Api.models;
 using Dsw2026Ej15.Domain.Entities;
 using Dsw2026Ej15.Domain.Exceptions;
+using Dsw2026Ej15.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dsw2026Ej15.Api.Controllers
 {
     [ApiController]
-    [Route("api/doctors")] 
+    [Route("api/doctors")]
     public class DoctorsController : ControllerBase
     {
         private readonly IPersistence _persistence;
-        public DoctorsController (IPersistence persistance)
+        public DoctorsController(IPersistence persistance)
         {
             _persistence = persistance;
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateDoctor([FromBody]DoctorModel.Request request)
-            
+        public async Task<IActionResult> CreateDoctor([FromBody] DoctorModel.Request request)
+
         {
-            if (string.IsNullOrWhiteSpace(request.Name)||string.IsNullOrWhiteSpace(request.LicenceNumber))
+            if (string.IsNullOrWhiteSpace(request.Name) || string.IsNullOrWhiteSpace(request.LicenceNumber))
             {
                 throw new ValidationException("Nombre y matricula son requeridos");
             }
@@ -40,7 +40,38 @@ namespace Dsw2026Ej15.Api.Controllers
                 Speciality = speciality
             };
             _persistence.doctors.Add(newDoctor);
-            return Created();           
+            return Created();
+        }
+        [HttpGet]
+           public async Task<IActionResult> GetDoctors()
+           {
+                var doctors = _persistence.doctors;
+                return Ok(doctors);
+           }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDoctorById(Guid id)
+        {
+            var doctor = _persistence.doctors.SingleOrDefault(d => d.Id == id);
+
+            if (doctor == null)
+            {
+                return NotFound("No se encontró ningún médico con el ID ingresado");
+            }
+            return Ok(doctor);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDoctor(Guid id)
+        {
+            var doctor = _persistence.doctors.SingleOrDefault(d => d.Id == id);
+
+            if (doctor == null)
+            {
+                return NotFound("No se encontró ningún médico con el ID ingresado para dar de baja");
+            }
+            doctor.IsActive = false;
+            return NoContent();
         }
     }
 }
